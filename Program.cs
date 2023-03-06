30cdbf6894d62a6fbfff33b87195e2a997af9afd
leno-tabJ0728 <116152759+leno-tabJ0728@users.noreply.github.com>
2023-03-06 11:24:31 +0500

    INFRA:Upload project

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+// System.Console.Write("\nIsmingizni kiriting : ");
+// string name = Console.ReadLine();
+// Console.WriteLine($"\nXush kelisiz {name}!\n");
+// Console.WriteLine("\nHisob kitobni boshlash uchun 1 ni kiriting!\n\nHisob kitob qilmasangiz 0 ni kiriting!");
+// Console.Write("\nHisob kitob qilamizmi : ");
+// int khisob=Convert.ToInt32(Console.ReadLine());
+// if (khisob>0)
+// {
+//         Console.Write("\nBirinchi sonni kiriting : ");
+//         double firstNamber=Convert.ToDouble(Console.ReadLine());
+//         Console.WriteLine("\nBajarishingiz mumkin bulgan amal ( + , - , * , / ,% )\n");
+//         Console.Write("Qanday amal bajaramiz : ");
+//         string should = Console.ReadLine();
+//         Console.Write("\nIkkinchi sonni kiriting : ");
+//         double secondNamber=Convert.ToDouble(Console.ReadLine());
+//         if (should=="+")
+//         {
+//                 Console.WriteLine($"\nIkki sonnning yig'indisi : {firstNamber+secondNamber}\n");
+//         }
+//         else if (should=="-")
+//         {
+//                 Console.WriteLine($"\nIkki sonning ayirmasi : {firstNamber-secondNamber}\n");
+//         }
+//         else if (should=="*")
+//         {
+//                 Console.WriteLine($"\nIkki sonning ko'paytmasi : {firstNamber*secondNamber}\n");
+//         }
+//         else if (should=="/")
+//         {
+//                 Console.WriteLine($"\nIkki sonning bulinmasi : {firstNamber/secondNamber}\n");
+//         }
+//         else if (should=="%")
+//         {
+//                 Console.WriteLine($"\nIkki sonni bulinmasida qoldiq : {firstNamber%secondNamber}\n");
+//         }
+// }
+// else if (khisob==0)
+// {
+//         Console.WriteLine("\nHisob kitob qilinmas ekan dasturni yopaman!\n");
+// }
+
+// //increment  degani + 1 bir qushib quy degani hisoblanadi
+// Console.WriteLine("\nIncrementni keltiramiz!");
+// Console.Write("\nBiror son kiriting : ");
+// double firstNamber=Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine($"\nKiritilgan sondan birga ortiq : {++firstNamber}\n");
+
+// //decrement deni - 1 birni ayiradi 
+// Console.WriteLine("Decrementni keltiramiz!");
+// Console.Write("\nBiror son kiriting : ");
+// double secondNamber=Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine($"\nKiritilgan sondan birga ortiq : {--secondNamber}\n");
+
+// Console.Write("\nBiror son kiriting : ");
+// int firstNamber=Convert.ToInt32(Console.ReadLine());
+// firstNamber/=10;
+// Console.WriteLine($"\nKiritilgan songa 10 qushadik va natija : {firstNamber}\n");
+
